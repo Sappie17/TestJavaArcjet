cf4f75af9c89758474782cc7adbbbbcafc747a26
Seppe Brosens <156705072+Sappie17@users.noreply.github.com>
2026-05-13 15:12:15 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int number = 5;
+        int square = number * number;
+
+        System.out.println("The square of " + number + " is " + square);
+    }
+}
